f26f9d157071a9df3ee365edbdf2ff9ebc8338df
Hery Fanomezantsoa <rhfano@gmail.com>
2022-01-31 23:02:35 +0300

    Change table shippers

--- database/migrations/2022_01_11_232931_add_status_date_updating_columns_to_transactions_table.php
+++ database/migrations/2022_01_11_232931_add_status_date_updating_columns_to_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddShippingDateUpdatingColumnsToTransactionsTable extends Migration
+class AddStatusDateUpdatingColumnsToTransactionsTable extends Migration
 {
     /**
      * Run the migrations.

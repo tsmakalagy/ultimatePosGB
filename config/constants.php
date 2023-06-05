4f082c6253dc1cd165237bee7f88fa671c2e9668
Hery Fanomezantsoa <rhfano@gmail.com>
2023-06-05 21:59:26 +0300

    Changed asset version

--- config/constants.php
+++ config/constants.php
@@ -33,7 +33,7 @@ return [
     'document_size_limit' => '5000000', //in Bytes,
     'image_size_limit' => '5000000', //in Bytes
 
-    'asset_version' => 45,
+    'asset_version' => 46,
 
     'disable_purchase_in_other_currency' => true,
     
